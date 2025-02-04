9a5e4804e8716b46357464ba51972aa9850620dd
Mariana Gomes <133662908+mariana-gomes021@users.noreply.github.com>
2025-02-04 20:52:02 -0300

    Add files via upload

---
+++ ExemploSaida.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class ExemploSaida
+{
+	public static void main(String[] args) {
+	    int idade = 17;
+		System.out.println("Hello World");
+		System.out.println("Minha idade é "+ idade + "anos");
+		System.out.println("Estou cursando Engenharia de Software.");
+		System.out.println("E me chamo Mariana.");
+	}
+}
