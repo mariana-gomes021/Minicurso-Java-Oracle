e4ea95799e9c77be378b318c19366eb75651f643
Mariana Gomes <133662908+mariana-gomes021@users.noreply.github.com>
2025-02-05 19:56:04 -0300

    Add files via upload

---
+++ Jogo1.java
@@ -0,0 +1,11 @@
+public class Jogo{
+  public static void main(String[] args){
+    int pontos;
+    pontos = 1000;
+    
+      if(pontos >= 1000){
+        System.out.println("Você passou para a fase 2 !!!");
+      }
+      System.out.println("próxima instrução!");
+  }
+}
